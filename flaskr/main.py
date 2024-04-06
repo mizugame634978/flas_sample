0c038d5c233d0bbbb880acb9d9f636b32d2e73e5
mizugame <ryouta1853@gamil.com>
2024-04-06 13:56:44 +0900

    チャプター：データ登録

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,5 +1,5 @@
-from flaskr import app
-from flask import render_template #faskrはフォルダ名
+from flaskr import app#faskrはフォルダ名
+from flask import render_template ,request,redirect,url_for
 import sqlite3
 DATABASE = 'db.sqlite3'
 
@@ -29,3 +29,22 @@ def form():
     return render_template(
         'form.html'
     )
+
+
+@app.route('/register',methods=["POST"])
+def register():
+    """form.htmlからデータを受け取って、DBに本を登録
+    
+    """
+    
+    title = request.form['title']
+    price = request.form['price']
+    arrival_day = request.form['arrival_day']
+
+    con = sqlite3.connect(DATABASE)
+    con.execute('INSERT INTO books VALUES(?,?,?)',
+                [title,price,arrival_day])
+    print("debug中")
+    con.commit()
+    con.close()
+    return redirect(url_for('index'))
